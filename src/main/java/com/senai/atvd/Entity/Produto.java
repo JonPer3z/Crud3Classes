8998e5cc183ed092791bb923815000cb2847ac2a
MykyBalb <mykaelbalbino06@gmail.com>
2025-03-17 16:38:23 -0300

    Correção Produto

--- src/main/java/com/senai/atvd/Entity/Produto.java
+++ src/main/java/com/senai/atvd/Entity/Produto.java
@@ -2,6 +2,8 @@ package com.senai.atvd.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +15,15 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Produto {
-
+    //
     @Id
     private Integer idProduto;
     private String nome;
     private String tipo;
     private Number preco;
+
+    @ManyToOne
+    @JoinColumn(name = "id_venda")
+    private Venda venda;
 
 }
